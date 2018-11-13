6ea6fe4619b3ccee1c6b345baa330fbc71b62282
srxgithub <43489203+srxgithub@users.noreply.github.com>
2018-11-13 00:56:46 -0200

    Adding comments in script

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+#PATH_INDEX is a variable that contains the path where the index.html file will be created
 PATH_INDEX="/var/www/html/"
+#PATH_INDEX is a variable that contains the path where the A.html file will be created
 PATH_A="${PATH_INDEX}A/"
+#read -r -d '' VAR << 'EOM' ... EOM reads multiple lines between EOM and stores in variable VAR
+#CONTENT_INDEX is the content of the index.html file
 read -r -d '' CONTENT_INDEX << 'EOM'
 <!DOCTYPE html>
 	<html>
@@ -10,6 +14,7 @@ read -r -d '' CONTENT_INDEX << 'EOM'
 		</body>
 	</html>
 EOM
+#CONTENT_A is the content of the A.html file
 read -r -d '' CONTENT_A << 'EOM'
 <!DOCTYPE html>
 	<html>
@@ -18,6 +23,9 @@ read -r -d '' CONTENT_A << 'EOM'
 		</body>
 	</html>
 EOM
+#mkdir -p creates the directory structure, if it does not exist
 mkdir -p ${PATH_A}
+#creates the index.html file with the proper predetermined content and path
 echo "$CONTENT_INDEX" > ${PATH_INDEX}index.html
+#creates the A.html file with the proper predetermined content and path
 echo "$CONTENT_A" > ${PATH_A}A.html
